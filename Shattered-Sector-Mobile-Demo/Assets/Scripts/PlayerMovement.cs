4a00ab0d7e4c77be1e6285d6b4dc82fa6739389e
Svilkata2020 <svilenmilenovmihailov2010@gmail.com>
2025-10-26 19:29:24 +0200

    fuckass unity

--- Shattered-Sector-Mobile-Demo/Assets/Scripts/PlayerMovement.cs
+++ Shattered-Sector-Mobile-Demo/Assets/Scripts/PlayerMovement.cs
@@ -9,12 +9,16 @@ public class PlayerMovement : MonoBehaviour
     private bool isMovingBackward = false;
     private bool isRotatingLeft = false;
     private bool isRotatingRight = false;
+    private bool isRotatingTopLeft = false;
+    private bool isRotatingTopRight = false;
 
     private Rigidbody rb;
+    private GameObject top;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        top = GameObject.FindWithTag("Top");
     }
 
     void FixedUpdate()
@@ -22,35 +26,77 @@ public class PlayerMovement : MonoBehaviour
         if (isMovingBackward)
         {
             StopMovingForward();
+            StopRotatingRight();
+            StopRotatingLeft();
             rb.linearVelocity = -transform.forward * moveSpeed;
         }
         if (isMovingForward)
         {
             StopMovingBackward();
+            StopRotatingRight();
+            StopRotatingLeft();
             rb.linearVelocity = transform.forward * moveSpeed;
         }
-        if(isRotatingLeft)
+
+        float rotationInput = 0f;
+        if (isRotatingLeft)
         {
+            StopMovingForward();
+            StopMovingBackward();
             StopRotatingRight();
-        }
-        if(isRotatingRight)
-        {
-            StopRotatingLeft();
+            rotationInput = -1f;
         }
 
+        if (isRotatingRight)
+        {
+            StopMovingForward();
+            StopMovingBackward();
+            StopRotatingLeft();
+            rotationInput = +1f;
+        }
+
+        if (rotationInput != 0f)
+        {
+            float rotationAmount = rotationInput * rotationSpeed * Time.fixedDeltaTime;
+            Quaternion delta = Quaternion.Euler(0f, rotationAmount, 0f);
+            rb.MoveRotation(rb.rotation * delta);
+        }
+
+        float topRotationInput = 0f;
+        if (isRotatingTopLeft)
+        {
+            StopRotatingTopRight();
+            topRotationInput = -1f;
+        }
+
+        if (isRotatingTopRight)
+        {
+            StopRotatingTopLeft();
+            topRotationInput = +1f;
+        }
+
+        if (topRotationInput != 0f)
+        {
+            top.transform.rotation *= Quaternion.Euler(0f, topRotationInput * rotationSpeed * Time.fixedDeltaTime, 0f);
+        }
+
+        transform.position = new Vector3(transform.position.x, 0, transform.position.z);
     }
 
     // Movement controls
     public void StartMovingForward() => isMovingForward = true;
     public void StopMovingForward() => isMovingForward = false;
-
     public void StartMovingBackward() => isMovingBackward = true;
     public void StopMovingBackward() => isMovingBackward = false;
 
     // Rotation controls
     public void StartRotatingLeft() => isRotatingLeft = true;
     public void StopRotatingLeft() => isRotatingLeft = false;
-
     public void StartRotatingRight() => isRotatingRight = true;
     public void StopRotatingRight() => isRotatingRight = false;
+    public void StartRotatingTopLeft() => isRotatingTopLeft = true;
+    public void StopRotatingTopLeft() => isRotatingTopLeft = false;
+    public void StartRotatingTopRight() => isRotatingTopRight = true;
+    public void StopRotatingTopRight() => isRotatingTopRight = false;
+
 }
